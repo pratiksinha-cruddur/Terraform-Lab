5680b993aa9a1e12343889d245d9b1e9c362c505
Pratik <pratik@mailinator.com>
2023-09-11 11:50:02 +0530

    Created S3 bucket by using depends on (Waited for EC2 to be created first)

---
+++ Practical_Lesson_4/Resource-Meta-Arguments-DependsOn/main.tf
@@ -0,0 +1,39 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "5.16.1"
+    }
+  }
+}
+
+provider "aws" {
+  region = "us-east-1"
+}
+
+resource "aws_instance" "my_server" {
+  //ami           = "ami-0f409bae3775dc8e5"
+  ami           = "ami-01c647eace872fc02"
+  instance_type = "t2.micro"
+}
+
+resource "aws_s3_bucket" "my_bucket" {
+  bucket = "my-tf-test-bucket-by-psh"
+  force_destroy = true
+  acl = "private"
+  depends_on = [
+    aws_instance.my_server
+  ]
+}
+
+output "public_ip" {
+  value = aws_instance.my_server.public_ip
+}
+
+/* output "s3_bucket_arn" {
+  value = aws_s3_bucket.my_bucket.s3_bucket_arn
+}*/
+
+/* output "s3_bucket_name" {
+  value = aws_s3_bucket.my_bucket.s3_bucket_id
+}  */
